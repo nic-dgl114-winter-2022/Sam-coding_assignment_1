c66b894fc566e1bbb8e1813eee9b91b9cb2f8298
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2022-02-09 00:27:24 -0800

    debugging

--- app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
+++ app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
@@ -2,20 +2,22 @@ package com.capstone.coding_assignment_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        value.setText("" + num)
+
+        value.text = "" + num
 
         plusBtn.setOnClickListener {
-            value.setText("" + ++num)
+            value.text = "" + ++num
         }
 
         minusBtn.setOnClickListener {
-            value.setText("" + --num)
+            value.text = "" + --num
         }
     }
 }
