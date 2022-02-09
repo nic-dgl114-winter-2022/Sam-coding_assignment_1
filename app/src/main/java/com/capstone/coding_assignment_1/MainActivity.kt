b57db7dcd4b188e871607af0c9bcf45d0f8a9dd2
Harsimranjit Kaur <harsimranchahal170@gmail.com>
2022-02-08 23:09:04 -0800

    setting on click action on value

--- app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
+++ app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
@@ -4,9 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        value.setText("" + num)
+
+        plusBtn.setOnClickListener {
+            value.setText("" + ++num)
+        }
+
+        minusBtn.setOnClickListener {
+            value.setText("" + --num)
+        }
     }
 }
