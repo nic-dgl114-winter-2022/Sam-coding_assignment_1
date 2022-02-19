b7bc6144725ce4e6d5929b4c971c9747fea87ab2
ash-teach <ashley.blacquiere@nic.bc.ca>
2022-02-18 16:41:52 -0800

    Feedback and comments

--- app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
+++ app/src/main/java/com/capstone/coding_assignment_1/MainActivity.kt
@@ -5,19 +5,19 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var num = 0
+    var number = 0 // In such a simple app no one would likely confuse 'num' for anything else, but you may as well be clear and use 'number'
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        value.text = "" + num
+        number_text.text = number.toString() //The "" + trick to turn a number into a string is useful in JavaScript but is not very idiomatic in terms of Kotlin. Prefer the use of the toString() method
 
-        plusBtn.setOnClickListener {
-            value.text = "" + ++num
+        plus_button.setOnClickListener {
+            number_text.text = (++number).toString() //as above - in this case you must wrap the ++number in parentheses for the toString() to operate on the entire expression
         }
 
-        minusBtn.setOnClickListener {
-            value.text = "" + --num
+        minus_button.setOnClickListener {
+            number_text.text = (--number).toString() //as above
         }
     }
 }
